216ad0feca0ad2ceb1b3b8409be551b593043260
Abubakar <adebayoabubakar41@gmail.com>
2022-06-02 17:28:39 -0700

    commit task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,17 +6,17 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-{ 
+{
 	char a;
 	int b;
 	long int c;
 	long long int d;
 	float g;
 
-       	printf("Size of char: %zu byte(s)\n", sizeof(a));
-	printf("Size of int: %zu byte(s)\n", sizeof(b));
-        printf("Size of long int: %zu byte(s)\n", sizeof(c));
-	printf("Size of long long int: %zu byte(s)\n", sizeof(d));
-	printf("Size of float: %zu byte(s)\n", sizeof(g));
+	printf("Size of a char: %zu byte(s)\n", sizeof(a));
+	printf("Size of an int: %zu byte(s)\n", sizeof(b));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(c));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(d));
+	printf("Size of a float: %zu byte(s)\n", sizeof(g));
 	return (0);
 }
