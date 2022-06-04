62012ae81f95a42a779c77aad18aca087cc5ce01
Abubakar <adebayoabubakar41@gmail.com>
2022-06-04 16:30:04 -0700

    commit task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return:Always 0.
+ */
+int main(void)
+{
+	char digit;
+
+	for( digit = 0; digit < 10; digit++)
+		putchar((digit%10 + 0));
+
+	putchar('\n')
+
+		return (0);
+}
