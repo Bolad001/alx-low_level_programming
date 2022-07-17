584dac77f9210af3632279666c7e0d6b730a9d29
Abubakar <adebayoabubakar41@gmail.com>
2022-07-17 16:52:08 -0700

    commit task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -5,7 +5,7 @@
 /**
  * print_to_98 - a function that prints all natural numbers from n to 98
  * user input's number prints to 98, regardless < 98 or > 98
- * @a: number input 
+ * @a: number input
  * Return: Always 0 (Success)
  */
 void print_to_98(int a)
