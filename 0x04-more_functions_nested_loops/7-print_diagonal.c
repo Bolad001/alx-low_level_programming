b81db23ebddc0d510c2de9267b6b6a0b36066580
Abubakar <adebayoabubakar41@gmail.com>
2022-08-14 16:36:18 -0700

    commit task 7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_diagonal - function that prints diagonal line in the terminal
+ * @n: number of times the line should be printed
+ * Return: return diagonal line
+ */
+
+void print_diagonal(int n)
+{
+	int a, b;
+
+	for (a = 0; a < n; a++)
+	{
+		for (b = 0; b < a; b++)
+		{
+			_putchar(' ');
+		}
+		_putchar('\\');
+
+		if (a < (n - 1))
+		{
+			_putchar('\n');
+		}
+	}
+	_putchar('\n'');
+
+}
