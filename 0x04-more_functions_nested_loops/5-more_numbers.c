6ac8841816d93e128abdbc0092bc765287f4689c
Abubakar <adebayoabubakar41@gmail.com>
2022-08-14 15:23:25 -0700

    commit task 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * more_numbers - a function that prints 10 times the number from 0 to 14
+ * Return: 0 followed by new line
+ */
+void more_numbers(void)
+{
+	int a, int b;
+
+	for (a = 0; a < 10; a++)
+	{
+		for (b = 0; b <= 14; b++)
+		{
+			if (b >= 10)
+			{
+				_putchar((b / 10) + '0');
+			}
+
+			_putchar((b % 10) + '0');
+
+		}
+		_putchar('\n');
+	}
+
+}
