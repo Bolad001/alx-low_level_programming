9e08f7ba62cbb990d91c1f1d2ff70df2f46ce4dc
Abubakar <adebayoabubakar41@gmail.com>
2022-08-15 15:24:39 -0700

    commit task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,7 +14,12 @@ int main(void)
 
 	for (a = 1; a <= 100; a++)
 	{
-		if (a % 3 == 0)
+		if  ((a % 3 == 0) && (a % 5 == 0))
+		{
+			printf(" FizzBuzz");
+		}
+
+		else if (a % 3 == 0)
 		{
 			printf(" Fizz");
 		}
@@ -22,11 +27,6 @@ int main(void)
 		else if (a % 5 == 0)
 		{
 			printf(" Buzz");
-		}
-
-		else if ((a % 3 == 0) && (a % 5 == 0))
-		{
-			printf(" FizzBuzz");
 		}
 
 		else
