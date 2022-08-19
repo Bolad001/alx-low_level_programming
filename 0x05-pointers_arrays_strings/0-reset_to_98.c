5312ca6224abce8b537116f1b22ddbbf547901d4
Abubakar <adebayoabubakar41@gmail.com>
2022-08-19 09:33:26 -0700

    commit task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - function that takes a pointer to int and update the value
+ * @n: input integer
+ * Retun: no return
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
