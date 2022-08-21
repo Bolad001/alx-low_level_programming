ddde3a364783c1f7439c0fa98102be9083b72838
Abubakar <adebayoabubakar41@gmail.com>
2022-08-20 17:54:08 -0700

    commit task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * print_rev - program that prints a string in reverse followed by new line
+ * @s: string input
+ * Return: no return
+ *
+ */
+void print_rev(char *s)
+{
+	int x;
+
+	for (x = 0; x <= 0; x++)
+	{
+		if ((s[x] == '\0'))
+		{
+			_putchar('\n');
+			break;
+		}
+		
+	for (x = s; x >= 0; x--)
+	{
+		_putchar(s[x]);
+		_putchar('\n');
+	}
+
+	}
+
+
+
+
+}
